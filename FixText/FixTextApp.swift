92929c30ccd4dd7c3e41fa838e306f648cfd2588
geraudd <geraud.daspremont@skai.io>
2024-08-26 13:03:08 +0200

    Init Swift Project

---
+++ FixText/FixTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixTextApp.swift
+//  FixText
+//
+//  Created by geraudd on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FixTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
